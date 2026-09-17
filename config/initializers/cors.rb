5a32e47884670e8ff70908ec0f6f0c21dd3bd6a6
kavya <kavya.actimize@gmail.com>
2026-09-17 16:27:09 +0530

    add

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,17 +1,24 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "http://localhost:3001", "http://localhost:5000"
+allow do
+origins(
+"http://localhost:3001",
+"http://localhost:5000",
+"https://task-19-628h.onrender.com"
+)
 
-    resource "/api/*",
-      headers: :any,
-      methods: [
-        :get,
-        :post,
-        :put,
-        :patch,
-        :delete,
-        :options,
-        :head
-      ]
-  end
+
+resource "/api/*",
+  headers: :any,
+  methods: [
+    :get,
+    :post,
+    :put,
+    :patch,
+    :delete,
+    :options,
+    :head
+  ]
+
+
+end
 end
