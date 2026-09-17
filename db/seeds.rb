8a847e374e1f8daeb4a02bb2399b38885600f3a6
kavya <kavya.actimize@gmail.com>
2026-09-17 17:10:46 +0530

    Add product seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,24 @@ AdminUser.find_or_initialize_by(email: "admin@gmail.com").tap do |admin|
   admin.password_confirmation = "Admin@123"
   admin.save!
 end
+require "json"
+
+products_file = Rails.root.join("products.json")
+
+if File.exist?(products_file)
+  products = JSON.parse(File.read(products_file))
+
+  products.each do |product|
+    Product.find_or_create_by!(id: product["id"]) do |p|
+      product.each do |key, value|
+        next if ["id", "created_at", "updated_at"].include?(key)
+
+        p[key] = value
+      end
+    end
+  end
+
+  puts "Imported #{products.count} products"
+else
+  puts "products.json not found"
+end
